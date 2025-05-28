a382614d5d67d4770e5ce4285c02e41155016961
Vlad Fediushin <vladfedyushin@gmail.com>
2025-05-28 08:37:58 +0400

    Moved logs.ts to /api

--- api/logs.ts
+++ api/logs.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method === 'POST') {
     const { message } = req.body
     console.log('[FRONT LOG]', message)
