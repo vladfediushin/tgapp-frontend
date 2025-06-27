bf6d0bc440c99e7a31175a113d27411e021a1bfa
Vlad Fediushin <vladfedyushin@gmail.com>
2025-06-28 01:10:08 +0400

    added global storage for locale

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+// src/main.tsx (или index.tsx)
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './i18n';
+import './i18n'
+import { useSession } from './store/session'
+import i18n from 'i18next'
+
+const Root: React.FC = () => {
+  const uiLang = useSession(state => state.uiLanguage)
+
+  useEffect(() => {
+    if (uiLang) {
+      i18n.changeLanguage(uiLang)
+    }
+  }, [uiLang])
+
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Root />
     </BrowserRouter>
   </React.StrictMode>,
 )
